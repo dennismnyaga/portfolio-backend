67af3b7aa96d0903f7fef95e74ec16b3f72b3a31
dennis nyaga <murildennis7@gmail.com>
2024-09-23 01:31:42 +0300

    added the media root

--- potfolio/settings.py
+++ potfolio/settings.py
@@ -136,9 +136,9 @@ STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-
-MEDIA_ROOT = BASE_DIR / 'static/images'
+# MEDIA_ROOT = BASE_DIR / 'static/images'
 
 
 
